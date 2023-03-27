a1ea2eef2016a42dadbaf51c6a3d9add45153592
devjoi2018 <devjoi2018@gmail.com>
2023-03-26 21:34:11 -0600

    Cambios en app_routes
    --- Se agrego la pantalla de login y registro a las rutas de navegacion

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,5 +3,7 @@ part of './app_pages.dart';
 abstract class Routes {
   static const splashPage = '/splashPage';
   static const startPage = '/startPage';
+  static const loginPage = '/loginPage';
+  static const registerPage = '/registerPage';
   static const homePage = '/homePage';
 }
